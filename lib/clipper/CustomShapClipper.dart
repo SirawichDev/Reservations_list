a4eeb04728c31794c64d5f92b4b98b548a964c59
SirawichDev <sirawit0676@gmail.com>
2019-05-05 08:13:09 +0700

    cliper update

--- lib/clipper/CustomShapClipper.dart
+++ lib/clipper/CustomShapClipper.dart
@@ -6,14 +6,18 @@ class CustomShapeClipper extends CustomClipper<Path>{
    final Path path = Path();
    path.lineTo(0.0, size.height);
 
-   var firstEndPoint = Offset(size.width * .5, size.height - 35.0);
-   var firstControlPoint = Offset(size.width * .25, size.height - 50.0);
-  
+   var firstEndPoint = Offset(size.width * .6, size.height - 35.0); //จุด2
+   var firstControlPoint = Offset(size.width * .3, size.height - 65.0); //จุด1
+
    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
 
-   var secondEndPoint = Offset(size.width,size.height - 80.0);
-   var secondControlPoint = Offset(size.width *.75, size.height - 10);
+   var secondEndPoint = Offset(size.width +30,size.height - 90.0);
+   var secondControlPoint = Offset(size.width *.9, size.height - 20);
    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy, secondEndPoint.dx, secondEndPoint.dy);
+//
+//   var thirdEndPoint = Offset(size.width,size.height -50.0);
+//   var thirdControlPoint = Offset(size.width * .40, size.height - 150);
+//   path.quadraticBezierTo(thirdControlPoint.dx, thirdControlPoint.dy, thirdEndPoint.dx, thirdEndPoint.dy);
    path.lineTo(size.width, 0.0);
    path.close();
    return path;
@@ -21,7 +25,7 @@ class CustomShapeClipper extends CustomClipper<Path>{
 
   @override
   bool shouldReclip(CustomClipper oldClipper) {
-    // TODO: implement shouldReclip
+    
     return true;
   }
 
