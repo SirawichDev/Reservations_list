6607795c9ef52b034ad0c1db030018a271d1b8ea
SirawichDev <sirawit0676@gmail.com>
2019-05-05 23:50:17 +0700

    dropdow

---
+++ lib/utils/DropDowns/DropDown.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const TextStyle dropDownLabelStyle =
+TextStyle(color: Colors.white, fontSize: 16.0);
+const TextStyle dropDownStyle = TextStyle(color: Colors.black, fontSize: 18.0);
+const TextStyle dropDownMenuStyle =
+TextStyle(color: Colors.black, fontSize: 20.0);
