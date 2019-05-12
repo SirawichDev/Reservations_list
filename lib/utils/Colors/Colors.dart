31ae6e4bcdb03740d47e3ada2546826f07dec4af
SirawichDev <sirawit0676@gmail.com>
2019-05-12 15:07:29 +0700

    .

--- lib/utils/Colors/Colors.dart
+++ lib/utils/Colors/Colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color fistColor = Colors.orange;
-const Color secColor = Colors.pink;
+const Color fistColor = Color.fromRGBO(138, 35, 135, 1);
+const Color secColor =Color.fromRGBO(233, 64, 87, 1);
+const Color thirdColor = Color.fromRGBO(242, 113, 33, 1);
